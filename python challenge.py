1dc4950268a072d909a1656ff0aaf679592d2530
Shaini Dasgupta <hottea.code@gmail.com>
2021-09-04 22:54:14 +0530

    first commit

---
+++ python challenge.py
@@ -0,0 +1,13 @@
+#level 2
+from urllib.request import urlopen
+#import string
+import re
+
+request = urlopen("http://www.pythonchallenge.com/pc/def/ocr.html")
+
+data = request.readlines()
+encrypted_message = map(lambda bytes: bytes.decode("utf-8"), data[37:1257])
+rare_chars = re.findall('[a-zA-Z]+', ''.join(encrypted_message))
+message = ''.join(rare_chars)
+
+print(message)
